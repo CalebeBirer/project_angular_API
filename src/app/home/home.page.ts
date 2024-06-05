ae7ea26dff5f29a8c32be05d4c8dae94ce10ef0e
Calebe Birer <calebeocanha53@gmail.com>
2024-06-05 08:52:21 -0300

    Create repository

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+ 
+  users: any[] = [];
+  selectUser: any;
 
-  constructor() {}
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit() {
+    this.apiService.getUsers().subscribe(
+      data => {
+        this.users = data;
+      });
+  }
+
+  onUseChange(event: any){
+    console.log('selected user:', event.detail.value);
+    this.selectUser = event.detail.value;
+  }
 }
